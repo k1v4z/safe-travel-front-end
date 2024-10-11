afc022e88803e665700273faa69a18b6ee87c3a7
k1v4z <addnguyen32gmail.com>
2024-10-11 13:39:30 +0700

    init auth provider

---
+++ safetravel/app/contexts/AuthProvider.tsx
@@ -0,0 +1,49 @@
+"use client"
+import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react'
+
+interface AuthContextType {
+    authenticated: boolean,
+    setAuth: (isAuth: boolean) => void,
+}
+
+interface AuthProviderProps {
+    children: ReactNode
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null)
+
+export default function AuthProvider({children}: AuthProviderProps){
+    const [authenticated, setAuth] = useState(false)
+
+    useEffect(() => {
+        //Check token in cookie
+        const checkToken = async() =>{
+            //Call api check token
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/check-token`, {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                'Content-Type': 'application/json',
+                }
+            });
+            
+            const data = await response.json()
+            
+            if(!data.haveToken){
+                setAuth(false)
+            }else{
+                setAuth(true)
+            }
+        }
+
+        checkToken()
+    }, [])
+
+  return (
+    <AuthContext.Provider value={{authenticated, setAuth}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+
