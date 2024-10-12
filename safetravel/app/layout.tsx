4b8ad4def524fb70d53c8175515f579a972ea620
k1v4z <addnguyen32gmail.com>
2024-10-12 10:03:00 +0700

    layout

--- safetravel/app/layout.tsx
+++ safetravel/app/layout.tsx
@@ -3,8 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import AuthProvider from "./contexts/AuthProvider";
 
-
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
