50815b4f5f2d798785c68de330091b33c4b71fac
k1v4z <addnguyen32gmail.com>
2024-10-12 10:01:42 +0700

    define section attribute

---
+++ safetravel/app/constants/sectionAttribute.ts
@@ -0,0 +1,37 @@
+export const functionAttributes = [
+    {
+        IMG_SRC: "/pictures/AI-builder-icon.png",
+        TEXT: `Get personalized </br> recs with AI`
+    },
+    {
+        IMG_SRC: "/pictures/favorite-icon.png",
+        TEXT: `Save hotels, restaurants, </br> and more`
+    },
+    {
+        IMG_SRC: "/pictures/map-icon.png",
+        TEXT: `See your saves </br> on your custom map`
+    },
+    {
+        IMG_SRC: "/pictures/collab-icon.png",
+        TEXT: `Share and collab with </br> your travel buds`
+    }
+]
+
+export const postAttribute = [
+    {
+        title: 'Start a trip in minutes with AI',
+        background: "/pictures/pic1.webp",
+        content: 'Answer four short questions and get personalized recs with AI, guided by traveler opinions.',
+        buttonText: 'Try AI Trip Builder',
+        buttonIcon: "/pictures/AI-icon.png"
+    },
+    {
+        title: 'Build your trip from scratch',
+        background: "/pictures/pic2.webp",
+        content: 'Browse top destinations, restaurants, and things to do and save your faves to your itinerary as you go.',
+        buttonText: 'Do it Yourself',
+        buttonIcon: "/pictures/trip-icon.png"
+    }
+]
+
+
