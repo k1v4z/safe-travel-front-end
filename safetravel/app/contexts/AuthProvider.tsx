cb187f13486ceb142b032910f803f45ae8ba5419
k1v4z <addnguyen32gmail.com>
2024-10-14 15:50:02 +0700

    add loading for check token

--- safetravel/app/contexts/AuthProvider.tsx
+++ safetravel/app/contexts/AuthProvider.tsx
@@ -1,49 +1,58 @@
-"use client"
-import React, { createContext, ReactNode, useEffect, useState } from 'react'
+"use client";
+import React, { createContext, ReactNode, useEffect, useState } from "react";
 
 interface AuthContextType {
-    authenticated: boolean,
-    setAuth: (isAuth: boolean) => void,
+  authenticated: boolean;
+  setAuth: (isAuth: boolean) => void;
+  isLoading: boolean;
 }
 
 interface AuthProviderProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-export const AuthContext = createContext<AuthContextType | null>(null)
+export const AuthContext = createContext<AuthContextType | null>(null);
 
-export default function AuthProvider({children}: AuthProviderProps){
-    const [authenticated, setAuth] = useState(false)
+export default function AuthProvider({ children }: AuthProviderProps) {
+  const [authenticated, setAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        //Check token in cookie
-        const checkToken = async() =>{
-            //Call api check token
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/check-token`, {
-                method: 'GET',
-                credentials: 'include',
-                headers: {
-                'Content-Type': 'application/json',
-                }
-            });
-            
-            const data = await response.json()
-            
-            if(!data.haveToken){
-                setAuth(false)
-            }else{
-                setAuth(true)
-            }
+  useEffect(() => {
+    //Check token in cookie
+    const checkToken = async () => {
+      try {
+        //Call api check token
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/check-token`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const data = await response.json();
+
+        if (!data.haveToken) {
+          setAuth(false);
+        } else {
+          setAuth(true);
         }
+      } catch (err) {
+        setAuth(false);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        checkToken()
-    }, [])
+    checkToken();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{authenticated,setAuth}}>
-        {children}
+    <AuthContext.Provider value={{ authenticated, setAuth, isLoading }}>
+      {children}
     </AuthContext.Provider>
-  )
+  );
 }
-
-
