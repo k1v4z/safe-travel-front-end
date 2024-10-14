943a6bfa18d8cae7479daa4e40fabc75d89d2223
k1v4z <addnguyen32gmail.com>
2024-10-14 15:54:30 +0700

    init about routing

---
+++ safetravel/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+
+const About = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default About
