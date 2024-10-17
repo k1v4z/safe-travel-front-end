f2381f3a040a6ad07f0f350eb08cc35c86296ad0
k1v4z <addnguyen32gmail.com>
2024-10-17 20:19:31 +0700

    handle fetch api plan

--- safetravel/app/plan/[id]/page.tsx
+++ safetravel/app/plan/[id]/page.tsx
@@ -4,24 +4,61 @@ import Footer from "../../components/Footer"
 import PlanTimeline from "../../components/Plan/PlanTimeline"
 import dynamic from "next/dynamic";
 import useAuth from "@/app/hooks/useAuth";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { PlansResponse } from "@/app/lib/contract/IActivity";
+
 
 const HereMap = dynamic(() => import('@/app/components/HereMap'), {
   ssr: false, // Disable ssr for HereMap component
 });
 
-const Plan = () => {  
-  useAuth()
 
+const Plan = () => {
+  const router = usePathname();
+  const [planData, setPlanData] = useState<PlansResponse | any>({})
+  const [coordinates, setCoordinates] = useState({latitude: 15.932077, longitude: 108.193575})
+  const updateMapLocation = (newLatitude: number, newLongitude: number) => {
+    setCoordinates({
+      latitude: newLatitude,
+      longitude: newLongitude
+    })
+  }
+
+  useAuth()
+  useEffect(() => {
+    const fetchingData = async () =>{
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${router}`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+
+      const {plans} = await response.json()
+      setPlanData(plans[0])
+    }
+
+    fetchingData()
+  },[])
+
+  
   return (
     <div className="bg-[#D2FBFD] min-h-screen">
       <Header />
       <div className="container mx-auto p-4 grid gap-6 md:grid-cols-1 lg:grid-cols-2">
         <div className="w-full">
-          <PlanTimeline />
+          <PlanTimeline updateMapLocation={updateMapLocation} plan={planData}/>
         </div>
         <div className="w-full h-auto">
-          <img className="w-full rounded-3xl border object-cover" src="/pictures/image 6.png" alt="Map of Hue" />
-          <HereMap/>
+          <img className="w-full rounded-3xl border object-cover" src={
+            planData.plan_on_province && planData.plan_on_province.map((planOnProvince: any) => {
+              return planOnProvince.province.imageUrl
+            })
+          } alt="Map of Hue" />
+          {/* Hard code temporarily */}
+          <HereMap latitude={coordinates.latitude} longitude={coordinates.longitude}/>
         </div>
       </div>
       <Footer />
