de16b6a72346368bcbe20625b430fc2ebd8c82e7
k1v4z <addnguyen32gmail.com>
2024-10-17 20:13:22 +0700

    update plan components

--- safetravel/app/components/Plan/ActivityItem.tsx
+++ safetravel/app/components/Plan/ActivityItem.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Image from "next/image";
-import { Activity } from "@/app/lib/contract/IActivity";
+import {  Activity } from "@/app/lib/contract/IActivity";
+import { formatDate } from "@/app/lib/formatDate";
 
 interface IActivityProps{
   activity: Activity,
-  isEdit: boolean
+  isEdit: boolean,
+  updateMapLocation(latitude: number, longitude: number): void
 }
 
-const ActivityItem = ({activity, isEdit}: IActivityProps) => {
+const ActivityItem = ({activity, isEdit, updateMapLocation}: IActivityProps) => {
+  
   return (
     <div
-      className="w-[75%] bg-white  ml-16 timeline-item flex py-4 px-6 mb-6 border border-[#c6c3c3] rounded-[20px] shadow-sm relative cursor-grabbing"
+      className="w-[75%] bg-white  ml-16 timeline-item flex py-4 px-6 mb-6 border border-[#c6c3c3] rounded-[20px] shadow-sm relative cursor-pointer"
     >
       {/* Circular marker */}
       <div className="absolute left-[-58px] top-1/2 transform -translate-y-1/2 bg-[#d9d9d9] w-[30px] h-[30px] rounded-full"></div>
@@ -20,31 +23,36 @@ const ActivityItem = ({activity, isEdit}: IActivityProps) => {
         width={!isEdit ? 20 : 8}
         height={!isEdit ? 20 : 8}
         alt=""
+        onClick={() => {
+          if(!isEdit){
+            updateMapLocation(Number(activity.activity_location.latitude), Number(activity.activity_location.longitude))
+          }
+        }}
       />
       {/* Timeline item content */}
       <div className="flex-shrink-0 mr-4">
         <img
           className="w-32 h-24 object-cover rounded-md"
-          src={activity.imageUrl}
-          alt={activity.title}
+          src={activity.activity_location.imageUrl}
+          alt={activity.activity_location.name}
         />
       </div>
       <div className="flex flex-col">
         <h2 className="text-lg font-gideonroman font-bold text-gray-800">
-          {activity.title}
+          {activity.activity_location.name}
         </h2>
         <p className="text-sm font-gideonroman text-[#0A1641]">
-          {activity.location}
+          {activity.activity_location.address}
         </p>
         <p className="text-sm font-gideonroman text-[#0A1641]">
-          {activity.address}
+          From: {formatDate(activity.start_date)} to {formatDate(activity.end_date)}
         </p>
         <p className="text-sm font-gideonroman text-[#0A1641]">
-          {activity.hours}
+          Open at: {activity.activity_location.open_at} &#x2022; Close at: {activity.activity_location.close_at}
         </p>
       </div>
       {
-        isEdit && <img className=" absolute w-[10%] h-[20%]  right-[-58px] top-14 cursor-grabbing" src="/pictures/move-icon.png" alt="" />
+        isEdit && <img className=" absolute w-[10%] h-[20%]  right-[-58px] top-14" src="/pictures/move-icon.png" alt="" />
       }
     </div>
   );
--- safetravel/app/components/Plan/PlanTimeline.tsx
+++ safetravel/app/components/Plan/PlanTimeline.tsx
@@ -1,46 +1,17 @@
 "use client"
 import { useState } from "react";
 import ActivityItem from "./ActivityItem";
-import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import { Plan, ProvincePlan} from "@/app/lib/contract/IActivity";
+import { formatDate } from "@/app/lib/formatDate";
 
-const initalActivities = [
-    {
-      id: 'abc',
-      imageUrl: '/pictures/154_hue 1.png',
-      title: 'The Imperial City of Hue',
-      address: 'Phu Hau, Hue City, Thua Thien Hue',
-      location: 'Hue',
-      hours: 'Open now • Closes at 17:00',
-    },
-    {
-      id: 'xyz',
-      imageUrl: '/pictures/154_hue 4.png',
-      title: 'Bun Bo Hue - O Be',
-      address: '77 Nguyen Hue, Phu Nhuan, Hue',
-      location: 'Hue',
-      hours: 'Open now • Closes at 12:30',
-    },
-    {
-      id: 'lop',
-      imageUrl: '/pictures/154_hue 8.png',
-      title: 'Thien An Hill',
-      address: 'Thuy Bang, Huong Thuy Town, Thua Thien Hue',
-      location: 'Hue',
-      hours: 'Open now',
-    },
-    {
-      id: '1sa',
-      imageUrl: '/pictures/154_hue 10.png',
-      title: 'Aeon Mall',
-      address: '08 Vo Nguyen Giap, An Duong Vuong, Hue',
-      location: 'Hue',
-      hours: 'Open now • Closes at 22:00',
-    },
-  ];
 
-const PlanTimeline = () => {
+interface IPlanTimeLineProps {
+  plan: Plan,
+  updateMapLocation(newLatitude: number, newLongitude: number): void
+}
+
+const PlanTimeline = (planTimeLineProps: IPlanTimeLineProps) => {
   const [isEdit, setIsEdit] = useState(false)
-  const [activities, setActivities] = useState(initalActivities)
 
   return (
     <div className="w-full overflow-hidden mt-1 p-6">
@@ -48,15 +19,19 @@ const PlanTimeline = () => {
       <div className="w-3/4 flex items-center  p-4 rounded-lg mb-6">
         <img className="w-16 h-16 mr-4" src="/pictures/target--shop-bullseye-arrow-target.png" alt="My Plan" />
         <div>
-          <h1 className="text-4xl font-gideonroman font-bold text-[#326D7B]">My Plan</h1>
-          <h2 className="text-3xl font-gideonroman font-bold text-[#9c3e3f]">HUE</h2>
-          <p className="text-sm font-gideonroman">Partner • October • 2 days</p>
+          <h1 className="text-4xl font-gideonroman font-bold text-[#326D7B]">{planTimeLineProps.plan.title}</h1>
+          <h2 className="text-3xl font-gideonroman font-bold text-[#9c3e3f]">{
+            planTimeLineProps.plan.plan_on_province && planTimeLineProps.plan.plan_on_province.map((province: ProvincePlan) => (
+              province.province.name
+            ))
+          }</h2>
+          <p className="text-sm font-gideonroman">{formatDate(planTimeLineProps.plan.date)}</p>
         </div>
       </div>
 
       {/* Plan Date and Edit Button */}
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-xl font-gideonroman text-black">Friday, Oct 25</h1>
+        <h1 className="text-xl font-gideonroman text-black">{formatDate(planTimeLineProps.plan.date)}</h1>
         <div className="flex flex-row gap-2 cursor-pointer">
           {
             isEdit && 
@@ -78,9 +53,10 @@ const PlanTimeline = () => {
         {/* Vertical line */}
         <div className="absolute left-[20px] top-0 h-full w-[1px] bg-black"></div>
         <div className="timeline h-[60vh] sm:h-[80vh] overflow-y-auto pr-2">
-          {activities.map((activity, index) => (
-            <ActivityItem  activity={activity} isEdit={isEdit} key={index}/>
+          {planTimeLineProps.plan.activities && planTimeLineProps.plan.activities.map((activity) => (
+            <ActivityItem updateMapLocation={planTimeLineProps.updateMapLocation} activity={activity} isEdit={isEdit} key={activity.id}/>
           ))}
+          
         </div>
       </div>
     </div>
