371250195bb9491226b906d0dddffbbc1ce77a40
k1v4z <addnguyen32gmail.com>
2024-10-17 20:19:04 +0700

    adjust dynamic location

--- safetravel/app/components/HereMap.tsx
+++ safetravel/app/components/HereMap.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useEffect } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+
 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -12,25 +13,32 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
-const HereMap = () => {
+interface IMapProps{
+  latitude: number,
+  longitude: number
+}
+
+const HereMap = ({latitude, longitude}: IMapProps) => {
   useEffect(() => {
     // Ensure that Leaflet only run in client
     if (typeof window === "undefined") return;
   }, []);
+  
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-auto">
       <MapContainer
-        center={[15.932077, 108.193575]}
-        zoom={13}
-        scrollWheelZoom={false}
+        center={[latitude, longitude]}
+        zoom={16}
         style={{ height: "400px", width: "100%" }}
+        key={`${latitude}-${longitude}`}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[15.932077, 108.193575]}>
+        {/* <LocationMarker/> */}
+        <Marker position={[latitude, longitude]} >
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
