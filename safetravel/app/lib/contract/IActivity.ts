61bf401a9e79ce3ffb8bf305e5974215ee909fa7
k1v4z <addnguyen32gmail.com>
2024-10-17 20:25:46 +0700

    update IActivity

--- safetravel/app/lib/contract/IActivity.ts
+++ safetravel/app/lib/contract/IActivity.ts
@@ -1,8 +1,39 @@
 export interface Activity {
     id: string,
-    imageUrl: string,
-    title: string,
-    address: string,
-    location: string,
-    hours: string
+    start_date: string,
+    end_date: string,
+    activity_location: ActivityLocation
+}
+
+export interface Plan {
+    title: string;
+    date: string;
+    activities: Activity[];
+    plan_on_province: ProvincePlan[];
+}
+
+
+export interface ActivityLocation {
+    id: string;
+    name: string;
+    address: string;
+    open_at: string;
+    close_at: string;
+    longitude: string;
+    latitude: string;
+    imageUrl: string;
+}
+
+export interface ProvincePlan {
+    province: Province;
+}
+
+interface Province {
+    name: string;
+    imageUrl: string;
+}
+
+
+export interface PlansResponse {
+    plans: Plan[];
 }
