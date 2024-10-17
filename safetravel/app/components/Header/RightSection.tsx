a34b9faadaaa66bcd52089c7e85c3a8e16a0e479
k1v4z <addnguyen32gmail.com>
2024-10-17 20:06:23 +0700

    add drop down profile

--- safetravel/app/components/Header/RightSection.tsx
+++ safetravel/app/components/Header/RightSection.tsx
@@ -1,18 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
 import { AuthContext } from "@/app/contexts/AuthProvider";
+import Link from "next/link";
+import DropDownProfile from "./DropDownProfile";
 const RightSection = () => {
   const authContext = useContext(AuthContext);
   let username = "";
   if (typeof window !== "undefined") {
     username = localStorage.getItem("username") || "";
   }
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleProfileMenu = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <div className="hidden md:flex items-center space-x-2">
-      <div className="font-poppins font-bold">
-        {authContext?.authenticated ? `Hi ${username}` : "My Account"}
-      </div>
       <div className="flex items-center space-x-4">
         <i className="fa fa-phone text-black text-lg"></i>
         <div className="font-poppins font-bold flex">
@@ -22,7 +26,7 @@ const RightSection = () => {
             width={25}
             height={25}
           />
-          +123456789
+          0354289547
         </div>
         <Image
           src={"/pictures/internet-icon.png"}
@@ -31,9 +35,19 @@ const RightSection = () => {
           alt=""
         />
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center gap-1">
+        <div className=" pl-2 font-poppins font-bold">EN</div>
         <i className="ri-global-line text-lg"></i>
-        <div className="border-l-2 pl-2 font-poppins font-bold">EN</div>
+       {
+        authContext?.authenticated ? 
+        <Image onClick={toggleProfileMenu} className="cursor-pointer border-l-2 pl-2" src={"/pictures/icons/general/logged-icon.png"} width={40} height={40} alt="avatar"/> :
+        <Link href={"/login"}>
+          <div className="border-l-2 pl-2 font-poppins font-bold cursor-pointer">Login</div>
+        </Link>
+       }
+       <div className="relative">
+          {isOpen && <DropDownProfile/>}
+       </div>
       </div>
     </div>
   );
