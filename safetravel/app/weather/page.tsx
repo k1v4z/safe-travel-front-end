d0c29e61d6a3b2aec27bbd27146b3f52bde1e29a
k1v4z <addnguyen32gmail.com>
2024-10-20 10:29:47 +0700

    use weather store

--- safetravel/app/weather/page.tsx
+++ safetravel/app/weather/page.tsx
@@ -1,11 +1,68 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer";
-import HereMap from "../components/HereMap";
 import SearchBar from "../components/SearchBar";
 import WeatherContainer from "../components/Weather/WeatherContainer";
+import useWeatherStore from "../stores/weatherStore";
+import dynamic from "next/dynamic";
+import useAirStore from "../stores/airStore";
+import useHourlyForecastStore from "../stores/hourlyForecastStore";
+import useDailyForecastStore from "../stores/dailyForecastStore";
+
+const HereMap = dynamic(() => import('@/app/components/HereMap'), {
+  ssr: false, // Disable ssr for HereMap component
+});
 
 const Weather = () => {
+  const [location, setLocation] = useState({lat: 15.932077, lng: 108.193575})
+  const {weather, setWeather} = useWeatherStore()
+  const {setAirQuality} = useAirStore();
+  const {setHourlyForecast} = useHourlyForecastStore()
+  const {setDailyForecast} = useDailyForecastStore()
+
+  useEffect(() => {
+    //Get currently location
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
+  //fetching weather data api
+  useEffect(() => {
+    const fetchWeather = async() => {
+      try{
+        const responseWeather = await fetch(`http://${process.env.NEXT_PUBLIC_SERVER}/weather?lat=${location.lat}&lng=${location.lng}&type=weather&domain=api.openweathermap.org`)
+        const responseAirQuality = await fetch(`http://${process.env.NEXT_PUBLIC_SERVER}/weather?lat=${location.lat}&lng=${location.lng}&type=air_pollution&domain=api.openweathermap.org`)
+        const responseHourlyForecast = await fetch(`http://${process.env.NEXT_PUBLIC_SERVER}/weather?lat=${location.lat}&lng=${location.lng}&type=forecast/hourly&domain=pro.openweathermap.org`)
+        const responseDailylyForecast = await fetch(`http://${process.env.NEXT_PUBLIC_SERVER}/weather?lat=${location.lat}&lng=${location.lng}&type=forecast/daily&domain=pro.openweathermap.org`)
+        const weatherData = await responseWeather.json()   
+        const airQualityData = await responseAirQuality.json()
+        const hourlyForecastData = await responseHourlyForecast.json()
+        const dailyForecastData = await responseDailylyForecast.json()
+        setWeather(weatherData)
+        setAirQuality(airQualityData)
+        setHourlyForecast(hourlyForecastData)
+        setDailyForecast(dailyForecastData)
+      }catch(err){
+        
+      }
+    }
+    fetchWeather()
+  },[])
+  
   return (
     <div className="min-h-screen bg-[#D2FBFD] flex flex-col">
       <Header />
@@ -17,9 +74,7 @@ const Weather = () => {
       <WeatherContainer/>
 
       {/* Map and Footer */}
-
-      {/* Hard code temporarily */}
-      <HereMap latitude={15.932077} longitude={108.193575}/> 
+      <HereMap latitude={location.lat} longitude={location.lng}/> 
       <Footer />
     </div>
   );
