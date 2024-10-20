09775ddd7b1cdd585ebbfd0a106fa36f3919dd8f
k1v4z <addnguyen32gmail.com>
2024-10-20 10:36:50 +0700

    use weather store

--- safetravel/app/components/Weather/WeatherTemp.tsx
+++ safetravel/app/components/Weather/WeatherTemp.tsx
@@ -1,19 +1,23 @@
+import { convertKToC } from "@/app/lib/convert";
+import useWeatherStore from "@/app/stores/weatherStore";
 import React from "react";
 
 const WeatherTemp = () => {
+  const { weather } = useWeatherStore()
+  
   return (
     <div className="lg:w-2/5 flex flex-col items-center lg:items-start space-y-4">
       <img
-        src="/pictures/icons//weather/cloudy-rain.png"
+        src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
         alt="Cloudy with Rain"
         className="w-72 lg:w-48"
       />
       <div>
         <h3 className="text-5xl font-gideonroman lg:text-6xl text-center lg:text-left">
-          27°
+          {convertKToC(weather?.main.temp || 0)}°
         </h3>
         <p className="text-xl ml-12 font-gideonroman text-black text-center lg:text-left">
-          Cloudy
+          {weather?.weather[0].main}
         </p>
       </div>
     </div>
