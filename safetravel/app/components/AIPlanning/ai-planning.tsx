ecf2f0a31f3701f0a2deb3c6a9641867a5529c75
k1v4z <addnguyen32gmail.com>
2024-11-22 01:14:19 +0700

    Add AI planning component and integrate with trip management

---
+++ safetravel/app/components/AIPlanning/ai-planning.tsx
@@ -0,0 +1,15 @@
+"use client"
+import usePlanStore from '@/app/stores/planStore';
+import React from 'react'
+
+const AIPlanning = () => {
+    const{plan, setPlan} = usePlanStore()
+    
+  return (
+    <div className='w-full min-h-screen'>
+      "Hello"
+    </div>
+  )
+}
+
+export default AIPlanning
--- safetravel/app/planning/page.tsx
+++ safetravel/app/planning/page.tsx
@@ -7,10 +7,14 @@ import MainContent from "../components/Planning/MakeTrip/MainContent";
 import DayChosen from "../components/DayChosen/DayChosen";
 import useAuth from "../hooks/useAuth";
 import KindChosen from "../components/KindChosen/KindChosen";
+import usePlanStore from "../stores/planStore";
+import AIPlanning from "../components/AIPlanning/ai-planning";
 
 const CreateTrip = () => {
   useAuth()
   const [step, setStep] = useState(0);
+  const { plan } = usePlanStore();
+  
   const handleBack = () => {
     setStep(step - 1);
   };
@@ -25,7 +29,8 @@ const CreateTrip = () => {
         {step === 0 && <ContainerCreation isNext={step > 0} setNext={(isNext) => setStep(isNext ? 1 : 0)} />}
         {step === 1 && <DayChosen onBack={handleBack} onNext={handleNext} />}
         {step === 2 && <KindChosen onBack={handleBack} onNext={handleNext} />}
-        {step === 3 && <MainContent />}
+        {step === 3 && !plan.isAITrip  && <MainContent />}
+        {step === 3 && plan.isAITrip && <AIPlanning />}
       <Footer />
     </div>
   );
